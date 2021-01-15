b16db2a34b9b694bd9ba5ec7f3dfb4e18c2b2293
travisyx <travisyx@gmail.com>
2021-01-15 05:49:00 -0500

    Tonight's progres

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-key = 563492ad6f91700001000001596501b32afd410da1a28b90e1b19b17
+import requests
+from bs4 import BeautifulSoup
+
+# Scrape the duckduckgo images for salad pictures
+url = "https://duckduckgo.com/?q=salad&t=ffab&atb=v1-1&iar=images&iax=images&ia=images"
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+temp = soup.find('img')
+print(temp)
+
+print(response.text)
 
