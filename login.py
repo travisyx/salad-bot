1d269d8e2d3b601c201e5f38a46a65628d7e5f71
travisyx <travisyx@gmail.com>
2021-01-18 04:32:07 -0500

    Able to login

--- login.py
+++ login.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.Firefox()
+# Open the web browser
+driver = webdriver.Chrome()
 driver.get("http://www.instagram.com")
+assert "Instagram" in driver.title
+
+# Log in (sleep to load the pages)
+time.sleep(4)
+username = driver.find_element_by_xpath("//input[@name='username']")
+username.send_keys("salad.robot")
+time.sleep(.1231)
+password = driver.find_element_by_xpath("//input[@name='password']")
+password.send_keys("travisscott")
+time.sleep(3)
+login = driver.find_element_by_xpath("//button[contains(.,'Log In')]")
+login.click()
 
